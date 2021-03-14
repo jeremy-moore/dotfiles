b3eecda2881b344e0615cf8fb9027241e78528e9
Jeremy Moore <jeremy-moore@users.noreply.github.com>
2021-03-15 10:55:55 +1100

    Fix .emacs.d

---
+++ .emacs.d/README.md
@@ -0,0 +1,13 @@
+# .emacs.d
+My Emacs configuration.
+
+## Emacs Installation
+
+```
+brew tap railwaycat/emacsmacport
+brew install --cask emacs-mac
+```
+
+See:
+- https://www.aidanscannell.com/post/setting-up-an-emacs-playground-on-mac/
+- https://ylluminarious.github.io/2019/05/23/emacs-mac-port-introduction/
---
+++ .emacs.d/init.el
@@ -0,0 +1,66 @@
+;; Basic Stuff.
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(menu-bar-mode -1)          ; Disable the menu bar
+(set-fringe-mode 10)        ; Give some breathing room
+
+;; Set up the visible bell.
+(setq visible-bell t)
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+
+;; Theme.
+(load-theme 'wombat)
+
+;; Font.
+(mac-auto-operator-composition-mode)
+(set-face-attribute 'default nil :font "Fira Code")
+
+
+;; Initialise package sources.
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialise use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package command-log-mode)
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
